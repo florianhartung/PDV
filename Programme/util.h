c8c21a7b3606c537eba3e0ea96957a601b65aba6
cxmu03 <=>
2022-10-14 10:21:00 +0200

    Used snprintf for string formatting as asprintf is not available

--- Programme/util.h
+++ Programme/util.h
@@ -7,8 +7,9 @@ constexpr size_t array_size(T arr[]) {
 
 template <typename... Ts>
 const char* format(const char* format_str, Ts... args) {
-  char* buffer;
-  asprintf(&buffer, format_str, args...);
+  size_t needed = snprintf(nullptr, 0, format_str, args...) + 1;
+  char buffer = malloc(needed);
+  sprintf(buffer, format_str, args...);
   return buffer;
 }
 
