9fe0553ca0ebba5890b1cb950e26e4f20b1fc19c
florianhartung <florian.hartung5@gmail.com>
2022-10-19 17:08:01 +0200

    Aufgabe 2.8

--- Aufgabenblatt2/programs/2.8.ino
+++ Aufgabenblatt2/programs/2.8.ino
@@ -1,9 +1,17 @@
-void setup8() {
-  // put your setup code here, to run once:
+#define LED_PWM_PIN 3
+#define DIP_SWITCH_START_INDEX 8
 
+void setup8() {
+  pins_set(DIP_SWITCH_START_INDEX, DIP_SWITCH_START_INDEX + 7, INPUT);
+  pinMode(LED_PWM_PIN, OUTPUT);
 }
 
 void loop8() {
-  // put your main code here, to run repeatedly:
+  // dip_read_word gibt wert von 0 bis 255 für die dip group `lower` zurück
+  int dip_value = dip_read_word(lower);
 
+  analogWrite(LED_PWM_PIN, dip_value);
+  
+  // Verzögerung, da sonst PWM nicht funktioniert
+  delay(50);
 }
