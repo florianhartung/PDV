86594994a7944e4aa23dc63b4b4b24134372a88f
florianhartung <florian.hartung5@gmail.com>
2022-10-26 16:21:19 +0200

    Code structure, removed Serial.print statements, removed usage of delay function

--- Aufgabenblatt3/program/program.ino
+++ Aufgabenblatt3/program/program.ino
@@ -1,24 +1,22 @@
 constexpr int motor_forward_pin = 3;
 constexpr int motor_backward_pin = 9;
-
 constexpr int direction_control_pin = 14;
 constexpr int speed_control_pin_from = 4;
 constexpr int speed_control_pin_to = 6;
 
-constexpr float acceleration = .1f/*s^(-1)*/;
+constexpr float acceleration = .1f/*(rad) / (s^-2)*/;
+constexpr int motor_update_interval = 5 /*ms*/;
 
-constexpr int update_threshold = 5 /*ms*/;
 
 struct MotorState {
-  int speed; //0-255
+  int speed; // 0 -255
   int target_speed;
 };
 
 
-unsigned long last_millis = 0;
+MotorState motor = { 0, 0 };
 unsigned long last_motor_update = 0;
-
-MotorState current_motor_state = { 0, 0 };
+bool force_next_update = false;
 
 
 void setup() {
@@ -27,7 +25,7 @@ void setup() {
   pinMode(motor_backward_pin, OUTPUT);
 
   pinMode(direction_control_pin, INPUT);
-
+  
   for(int pin = speed_control_pin_from; pin <= speed_control_pin_to; pin++) {
     pinMode(pin, INPUT);    
   }          
@@ -35,25 +33,26 @@ void setup() {
 
 void loop() {
   int new_motor_speed = read_new_motor_speed();
-
-  Serial.print("Current: "); Serial.print(current_motor_state.speed); Serial.print("; New: "); Serial.println(new_motor_speed);
-  if (current_motor_state.target_speed != new_motor_speed) {
-    last_motor_update = millis();
-    current_motor_state.target_speed = new_motor_speed;
+  if (motor.target_speed != new_motor_speed) {
+    motor.target_speed = new_target_speed;
+    force_next_update = true;
   }
 
-  // Alle update_threshold millisekunden delayen
-  if (current_motor_state.speed != current_motor_state.target_speed) {
-    int updated_motor_speed = interpolate_motor_speeds(current_motor_state.speed, current_motor_state.target_speed);
-    Serial.print("Updated Motor Speed: "); Serial.println(updated_motor_speed);
-    last_motor_update = millis();
-    current_motor_state.speed = updated_motor_speed;
 
-    change_motor_speed(current_motor_state.speed);    
+  auto delta_time = millis() - last_motor_update;
+  if (delta_time >= motor_update_interval || force_next_update) {
+    if (motor.speed != motor.target_speed) {
+      update_motor_speed(motor, delta_time);
+      change_motor_speed(motor.speed);    
+    }
+    
+    force_next_update = false;
+    last_motor_update += delta_time;
+
+    /// TODO: Serial Plotter output
   }
-  Serial.print("Speed: "); Serial.print(current_motor_state.speed); Serial.print("; Target: "); Serial.println(current_motor_state.target_speed);
-  delay(update_threshold);
 }
+
 
 int read_new_motor_speed() {
   int new_dip_speed = 0;
@@ -62,39 +61,45 @@ int read_new_motor_speed() {
     new_dip_speed |= digitalRead(pin);
   }
   int new_motor_speed = (new_dip_speed * 255) / 7;
-  Serial.println(new_motor_speed);
+
   return digitalRead(direction_control_pin) ? -new_motor_speed : new_motor_speed;
 }
 
-int interpolate_motor_speeds(int speed_from, int speed_to) {
-    unsigned long delta_time = millis() - last_motor_update;
-    Serial.print("Delta Time: "); Serial.println(delta_time);
-    int direction = sign(speed_to - speed_from);
+void update_motor_speed(MotorState motor, int delta_time) {
+    int speed_to = motor.target_speed;
+    int speed_from = motor.speed;
     
-    int new_speed = speed_from + direction * (static_cast<float>(delta_time) * acceleration);
-      
-    if(direction == 1 && new_speed > speed_to || direction == -1 && new_speed < speed_to) {
-      return speed_to;
-    }
-
-    return new_speed;
+    int new_speed = linear_interpolation(speed_from, speed_to, delta_time);
+    motor.speed = new_speed;
 }
 
 void change_motor_speed(int speed) {
-  Serial.print("Speedo "); Serial.println(speed);
   analogWrite(motor_forward_pin, speed > 0 ? speed : 0);
   analogWrite(motor_backward_pin, speed < 0 ? -speed : 0);
 }
 
+/**
+ * Linear interpolation with a set slope specified by @link{acceleration}.
+ *
+ * @param from First interpolation value
+ * @param to Second interpolation value
+ * @param delta_time Time passed since first interpolation value 
+ *
+ * @returns The interpolated value
+ */
+int linear_interpolation(int from, int to, int delta_time) {
+  int direction = sign(to - from);
+
+  int result = from + direction * (delta_time * acceleration);
+  
+  if (from < to && result > to || from > to && result < to) {
+    result = to;
+  }
+
+  return to;
+}
 
 template <typename T>
 constexpr int sign(T t) {
   return t < 0 ? -1 : 1; 
 }
-/*
-auto current_millis = millis();
-  if (current_millis - last_millis >= update_threshold) {
-    /*
-    last_millis = current_millis;
-  }
-*/
